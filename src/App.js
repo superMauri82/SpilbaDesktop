03916814dca1e5f88da5bbc942d48f41d28fae2e
Mauricio <maurito11@gmail.com>
2018-03-11 14:56:54 -0300

    Commit Inicial - Estructura Basica de la App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/LOGO_SPILBA.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+          <h2>Welcome to Spilba</h2>
         </div>
         <p className="App-intro">
-          Hello Electron!
+          Spilba Electron!
         </p>
       </div>
     );
