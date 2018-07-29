79732658d9ad93bfd065876bdf3657cdaa193147
Mauricio Costa <maurito11@gmail.com>
2018-07-29 12:52:24 -0300

    Ejectuable Linux. Primer Intento.

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -13,7 +13,11 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({
+        width: 800, 
+        height: 600,
+        icon: path.join(__dirname,'images/logo.png')
+    });
 
     // and load the index.html of the app.
     const startUrl = process.env.ELECTRON_START_URL || url.format({
@@ -23,7 +27,7 @@ function createWindow() {
         });
     mainWindow.loadURL(startUrl);
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
